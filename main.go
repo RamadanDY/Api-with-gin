c5aed03978db0420ae20c3d4d85221bccbdd01c5
RamDev <daudalaramadan2@gmail.com>
2026-02-28 00:53:21 +0000

    fix: correct package declaration and format code in main.go

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 pakage main
+
+
+////we will be working on an api that will be able to 
+////store ,retrieve and change data 
+
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"errors"
+)
+
+
+type book struct {
+	ID string
+	Title string
+	Author string
+	Quantity int
+}
